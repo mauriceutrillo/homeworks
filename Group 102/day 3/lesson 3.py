32fe6845ae570454877ec2908af05addb6024427
mauriceutrillo <119354818+mauriceutrillo@users.noreply.github.com>
2022-11-30 00:00:59 +0400

    day 3 commit

---
+++ Group 102/day 3/lesson 3.py
@@ -0,0 +1,55 @@
+# my_name = "david"
+
+# if "a" in my_name:
+#     print("sheicavs a-s")
+
+# my_age = 26
+# my_adress = "kavtaradze str"
+# my_weight = 100
+
+
+# my_sentence = "aa {} bb {} cc {}".format(my_age, my_adress, my_adress)  #ფორმატირება, რკალების ადგილას ჩავსვით სხვა მონაცემები
+
+# print(my_sentence)
+
+# my_name = "david" 
+# my_surname = "khijakadze"
+# my_age = 26
+# my_education = "programming"
+
+# all_in_one = "i am {} {}, my age is {}, i am studying {}".format(my_name, my_surname, my_age, my_education)
+
+# print(all_in_one)
+
+
+#ამ კოდით ტერმინალზე გამოდის მესიჯი, ხოლო იქ მითითებული ინფორმაცია input მეთოდის საშუალებით ცვლადში, მაგალითად (my_name), შეინახება
+
+# my_name = input("enter your name: ")  
+# print("my name is " + my_name)
+
+# my_age = 26
+# my_age +=3
+
+# print(my_age)
+
+# name = input("enter your name: ")
+# surname = input("enter your surname: ")
+# age = int(input("enter your age: "))
+
+# print("your name is {}, your surname is {}, your age is {}".format(name,surname,age))
+
+# age += 3
+
+# print("after 3 years i am {} years old".format(age))
+
+
+# num1 = int(input("enter any numer: "))
+# num2 = int(input("enter any number: "))
+
+# product = num1*num2
+
+# if product > 100:
+#     print(product)
+# else:
+#     print("you lose")
+
