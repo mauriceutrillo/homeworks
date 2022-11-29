2dd8121f8f4b7086d12557ed1dd81642746e54a0
mauriceutrillo <119354818+mauriceutrillo@users.noreply.github.com>
2022-11-29 20:46:58 +0400

    day 2 commit

---
+++ Group 102/day 2/lesson 2.py
@@ -0,0 +1,22 @@
+""" name = "David"
+
+print(len(name)) #len - lenght ფუნქცია ითვლის ასოთა რაოდენობას """
+
+full_name = "David khijakadze"
+
+#print(len(full_name))
+
+#print(full_name[4])
+
+#print("kh" in full_name)
+
+# print(full_name[2:8])
+# print(full_name[2:8:2])
+#start:finish:step
+
+# print(full_name.replace("d","#"))  #ჩანაცვლება
+
+
+
+
+
